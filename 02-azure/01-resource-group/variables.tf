6e9bfb0b4934dc9355eeb606b9313961e34a7ab2
JoseVelazquez-Labs <josevelazquezexposito@gmail.com>
2025-09-30 18:57:14 +0200

    Update variables.tf

--- 02-azure/01-resource-group/variables.tf
+++ 02-azure/01-resource-group/variables.tf
@@ -1,20 +1,23 @@
+# Nombre del Resource Group
 variable "rg_name" {
-  description = "Nombre del Resource Group"
-  type        = string
-  default     = "rg-lab-terraform"
+  description = "Nombre del Resource Group"   
+  type        = string                        # Tipo de dato: texto.
+  default     = "rg-lab-terraform"            # Valor por defecto (puedes cambiarlo).
 }
 
+# Región donde desplegar los recursos.
 variable "location" {
-  description = "Región de Azure"
+  description = "Región de Azure"             # Ejemplo: westeurope, northeurope, etc.
   type        = string
-  default     = "westeurope"
+  default     = "westeurope"                  # Valor por defecto (Europa Oeste)
 }
 
+# Etiquetas para aplicar al grupo de recursos. (clave → valor)
 variable "tags" {
   description = "Etiquetas comunes"
-  type        = map(string)
+  type        = map(string)                   # Tipo: mapa de pares clave-valor
   default = {
-    owner = "JoseVelazquez"
-    env   = "lab"
+    owner = "JoseVelazquez"                   # Quién creó el recurso
+    env   = "lab"                             # Entorno (lab, dev, prod)
   }
 }
