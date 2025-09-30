5df1a7de899a7f4387a2a204c346f182f7cd5d15
JoseVelazquez-Labs <josevelazquezexposito@gmail.com>
2025-09-30 18:55:35 +0200

    Update main.tf

--- 02-azure/01-resource-group/main.tf
+++ 02-azure/01-resource-group/main.tf
@@ -1,19 +1,20 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5.0"   # Versión mínima de Terraform necesaria
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      source  = "hashicorp/azurerm"   # El proveedor es Microsoft Azure.
+      version = "~> 3.100"            
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {}   # Las características son una configuración obligatoria para Azure, pero se dejan vacías.
 }
 
+# Definición del Resource Group en Azure
 resource "azurerm_resource_group" "rg" {
-  name     = var.rg_name
-  location = var.location
-  tags     = var.tags
+  name     = var.rg_name   # Nombre del grupo de recursos (lo tomamos de variables.tf).
+  location = var.location  # Región donde se creará. (lo tomamos de variables.tf).
+  tags     = var.tags      # Etiquetas. (lo tomamos de variables.tf).
 }
